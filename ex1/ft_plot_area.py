815aed9bdc1a260a3bbccca0a42ef2f589680486
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2026-03-22 14:17:58 +0100

    ft_plot_area

--- ex1/ft_plot_area.py
+++ ex1/ft_plot_area.py
@@ -1,4 +1,4 @@
 def ft_plot_area():
-	x = int(input('Enter length: '))
-	y = int(input('Enter width: '))
-	print('Plot area: ' + x * y)
+	x = int(input("Enter length: "))
+	y = int(input("Enter width: "))
+	print("Plot area: ", x * y)
