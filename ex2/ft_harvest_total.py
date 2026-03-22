7226b4434525f6e24241504aa814c2d97cc2e81a
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2026-03-22 14:46:51 +0100

    ft_harvest_total

---
+++ ex2/ft_harvest_total.py
@@ -0,0 +1,6 @@
+def ft_harvest_total():
+	x = int(input("Day 1 harvest: "))
+	y = int(input("Day 2 harvest: "))
+	z = int(input("Day 3 harvest: "))
+	total = x + y + z
+	print("Total harvest: ", total)
