e255299c642b6a18df88ae20507336eed0a6dc0d
Koodal Kweon <kkweon@f0r4s14.codam.nl>
2026-03-23 14:16:24 +0100

    ft_count_harvest_recursive

--- ex5/ft_count_harvest_recursive.py
+++ ex5/ft_count_harvest_recursive.py
@@ -1,4 +1,10 @@
 def ft_count_harvest_recursive():
-	harvest_day = int(input("Days until harvest: "))
-	day = 1
-	if ()
+	day = int(input("Days until harvest: "))
+	recursive_helper(1, day)
+
+def recursive_helper(count, day):
+	if count > day:
+		print("Harvest time!")
+	else:
+		print("Day", count)
+		recursive_helper(count + 1, day)
