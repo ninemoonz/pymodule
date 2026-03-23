2c3fdcbdeefdd8e317d5867614be3710fe9cfba6
Koodal Kweon <kkweon@f0r4s14.codam.nl>
2026-03-23 14:41:39 +0100

    ft_garden_summary & ft_seed_inventory

---
+++ ex6/ft_garden_summary.py
@@ -0,0 +1,6 @@
+def ft_garden_summary():
+    name = input("Enter garden name: ")
+    plant = int(input("Enter number of plants: "))
+    print("Garden: ", name)
+    print("Plants: ", plant)
+    print("Status: Growing well!")
---
+++ ex7/ft_seed_inventory.py
@@ -0,0 +1,2 @@
+def ft_seed_inventory(seed: str, quantity: int, unit: str):
+    print(seed, "seeds:", quantity, unit, "available")
