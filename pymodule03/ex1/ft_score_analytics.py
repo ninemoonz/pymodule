b5d8128968c7929d0ec19df4f8e36c58c2f552dd
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2026-04-16 10:19:48 +0200

    i think it s done

--- pymodule03/ex1/ft_score_analytics.py
+++ pymodule03/ex1/ft_score_analytics.py
@@ -19,16 +19,16 @@ class NonNumericError(ScoreError):
         super().__init__(message)
 
 
-def no_arg_test(arg_list: list) -> list:
+def no_arg_test(arg_list: list) -> None:
     if len(arg_list) < 2:
-        raise NoArgError("No Scores Provided")
-    return arg_list
+        raise NoArgError("No Scores Provided.")
 
 
-def non_numeric_test(arg: str) -> list:
-    if not int(arg):
-        raise NonNumericError(f"Invalid parameter:\'{arg}\'")
-    return arg
+def non_numeric_test(arg: str) -> None:
+    try:
+        int(arg)
+    except ValueError:
+        raise NonNumericError(f"Invalid parameter:'{arg}'")
 
 
 def score_process(arg_list: list) -> list:
@@ -36,7 +36,7 @@ def score_process(arg_list: list) -> list:
     try:
         no_arg_test(arg_list)
     except NoArgError as e:
-        print(f"{e}. Usage: python3 ft_score_analytics.py <score1> <score2> ...")
+        print(f"{e} Usage: python3 ft_score_analytics.py <score1> <score2> ...")
 
     for i in range(1, len(arg_list)):
         try:
@@ -44,9 +44,28 @@ def score_process(arg_list: list) -> list:
         except ScoreError as e:
             print(f"{e}")
         else:
-            score_list.append(arg_list[i])
+            score_list.append(int(arg_list[i]))
+    return score_list
 
 
 if __name__ == "__main__":
     arg_list = sys.argv
-    processed_list = score_process(arg_list)
+    processed_list: list = []
+    print("=== Player Score Analytics ===")
+    if score_process(arg_list):
+        processed_list = score_process(arg_list)
+        total_player = len(processed_list)
+        total_score = sum(processed_list)
+        average_score = total_score / total_player
+        high_score = max(processed_list)
+        low_score = min(processed_list)
+        score_range = high_score - low_score
+
+        print(f"Scores Processed: {processed_list}")
+        print(f"Total players: {total_player}")
+        print(f"Total score: {total_score}")
+        print(f"Average score: {average_score:.1f}")
+        print(f"High score: {high_score}")
+        print(f"Low score: {low_score}")
+        print(f"Score range: {score_range}")
+    print()
