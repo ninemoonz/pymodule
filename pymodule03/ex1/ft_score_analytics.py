8f7eb34de86dc853f23293812b3d7af2d1ec0602
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2026-04-16 10:54:52 +0200

    finish

--- pymodule03/ex1/ft_score_analytics.py
+++ pymodule03/ex1/ft_score_analytics.py
@@ -2,33 +2,30 @@ import sys
 
 
 class ScoreError(Exception):
-    def __init__(self, message: str):
+    def __init__(self, message: str = "No Scores Provided. Usage: "
+                 "python3 ft_score_analytics.py <score1> <score2> ..."):
         super().__init__(message)
         self.message = message
 
 
 class NoArgError(ScoreError):
-    def __init__(self, message: str):
-        super().__init__(message)
-        self.message = message
+    pass
 
 
 class NonNumericError(ScoreError):
-    def __init__(self, message: str):
-        self.message = message
-        super().__init__(message)
+    pass
 
 
 def no_arg_test(arg_list: list) -> None:
     if len(arg_list) < 2:
-        raise NoArgError("No Scores Provided.")
+        raise NoArgError()
 
 
 def non_numeric_test(arg: str) -> None:
     try:
         int(arg)
     except ValueError:
-        raise NonNumericError(f"Invalid parameter:'{arg}'")
+        raise NonNumericError(f"Invalid parameter: '{arg}'")
 
 
 def score_process(arg_list: list) -> list:
@@ -36,15 +33,20 @@ def score_process(arg_list: list) -> list:
     try:
         no_arg_test(arg_list)
     except NoArgError as e:
-        print(f"{e} Usage: python3 ft_score_analytics.py <score1> <score2> ...")
+        print(e)
+        return score_list
 
     for i in range(1, len(arg_list)):
         try:
             non_numeric_test(arg_list[i])
         except ScoreError as e:
-            print(f"{e}")
+            print(e)
         else:
             score_list.append(int(arg_list[i]))
+
+    if not score_list:
+        print(ScoreError())
+
     return score_list
 
 
@@ -52,8 +54,8 @@ if __name__ == "__main__":
     arg_list = sys.argv
     processed_list: list = []
     print("=== Player Score Analytics ===")
-    if score_process(arg_list):
-        processed_list = score_process(arg_list)
+    processed_list = score_process(arg_list)
+    if processed_list:
         total_player = len(processed_list)
         total_score = sum(processed_list)
         average_score = total_score / total_player
