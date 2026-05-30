259008d3e7bb21fc2df0dd3090e1bcffb093d009
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2026-05-30 15:17:36 +0200

    OSError

--- pymodule04/ex0/ft_ancient_text.py
+++ pymodule04/ex0/ft_ancient_text.py
@@ -6,3 +6,17 @@ if __name__ == "__main__":
     arg_input = sys.argv
     if len(arg_input) == 1:
         print(f"Usage: {arg_input[0]} <file>")
+    else:
+        file_name: str = arg_input[1]
+        f: typing.IO[str] | None = None
+        print("=== Cyber Archive Recovery ===")
+        print(f"Accessing file '{file_name}'")
+        try:
+            f = open(file_name, "r")
+            print(f.read())
+        except OSError as e:
+            print(f"Error opening file '{file_name}': {e}")
+        finally:
+            if f:
+                f.close()
+                print(f"File '{file_name}' closed.")
