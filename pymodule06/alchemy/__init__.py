ecfe0020653671808e11c07a44169f0101eb5e71
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2026-06-21 23:17:01 +0200

    asdfadsf

--- pymodule06/alchemy/__init__.py
+++ pymodule06/alchemy/__init__.py
@@ -1,3 +1,4 @@
 from .elements import create_air
 from .potions import healing_potion as heal
 from .potions import strength_potion
+from .transmutation import lead_to_gold
---
+++ pymodule06/ft_transmutation_2.py
@@ -0,0 +1,5 @@
+import alchemy
+
+print("=== Transmutation 2 ===")
+print("Importing alchemy module only")
+print(f"Testing lead to gold: {alchemy.lead_to_gold()}")
