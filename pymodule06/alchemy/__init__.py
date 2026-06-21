485965da3ce410bea3ced87a3e79819d6fe9e7ba
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2026-06-21 22:07:21 +0200

    distillation

--- pymodule06/alchemy/__init__.py
+++ pymodule06/alchemy/__init__.py
@@ -1,1 +1,3 @@
-from alchemy.elements import create_air
+from .elements import create_air
+from .potions import healing_potion as heal
+from .potions import strength_potion
---
+++ pymodule06/ft_distillation_0.py
@@ -0,0 +1,6 @@
+from alchemy import potions
+
+print("=== Distillation 0 ===")
+print("Direct access to alchemy/potions.py")
+print(potions.healing_potion())
+print(potions.strength_potion())
