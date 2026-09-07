244179477021b91db21efbc55f2c97938094799c
Ladook <76804681+ninemoonz@users.noreply.github.com>
2026-09-07 17:30:29 +0200

    fix error

--- py_module_07/ex2/battle_strategy.py
+++ py_module_07/ex2/battle_strategy.py
@@ -5,8 +5,8 @@ from ex0.creature_abc import Creature
 class BattleStrategy(ABC):
     @abstractmethod
     def act(self, creature: Creature) -> str:
-        ...
+        pass
 
     @abstractmethod
     def is_valid(self, creature: Creature) -> bool:
-        ...
+        pass
