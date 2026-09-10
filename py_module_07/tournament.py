2f678f6129ade1a51f98ccfba6f770dda9818a60
Ladook <76804681+ninemoonz@users.noreply.github.com>
2026-09-10 16:10:56 +0200

    tournament

--- py_module_07/tournament.py
+++ py_module_07/tournament.py
@@ -1,4 +1,3 @@
-from itertools import combinations
 from typing import List, Tuple
 from ex0 import CreatureFactory, FlameFactory, AquaFactory
 from ex1 import HealingCreatureFactory, TransformCreatureFactory
@@ -19,20 +18,6 @@ def battle(opponents: List[Opponent]) -> None:
     fighters = [
         (factory.create_base(), strategy) for factory, strategy in opponents
     ]
-    for ((creature_a, strategy_a),
-         (creature_b, strategy_b)) in combinations(fighters, 2):
-        print("* Battle *")
-        print(creature_a.describe())
-        print(" vs. ")
-        print(creature_b.describe())
-        print(" Now Fight!")
-        try:
-            print(strategy_a.act(creature_a))
-            print(strategy_b.act(creature_b))
-        except StrategyError as e:
-            print(f"Battle Error, aborting tournament: {e}")
-            return
-        print()
 
 
 if __name__ == "__main__":
