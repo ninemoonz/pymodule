9acb6848d98fb45debdb3fa6492bbdbd1c5c734c
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2026-09-13 20:04:23 +0200

    construct

--- py_module_08/ex0/construct.py
+++ py_module_08/ex0/construct.py
@@ -16,11 +16,44 @@ def matrix_stat(venv: bool) -> str:
     if not venv:
         message = "You're still plugged in"
     else:
-        message = "welcome to the construct"
+        message = "Welcome to the construct"
     matrix_msg = f"MATRIX STATUS: {message}"
     return matrix_msg
 
 
+def current_path() -> str:
+    return sys.prefix
+
+
+def package_site_path(venv: bool) -> str:
+    if not venv:
+        return ("To enter the construct, run:\n"
+                "python -m matrix_env\n"
+                "source matrix_env/bin/activate # On Unix\n"
+                "matrix_env\\Scripts\\activate # On Windows\n"
+                "\n"
+                "Then run this program again.")
+    else:
+        return ("Package installation path:\n"
+                f"{site.USER_SITE}")
+
+
+def confirm_message(venv: bool) -> str:
+    if not venv:
+        return ("WARNING: You're in the global environment!\n"
+                "The machines can see everything you install")
+    else:
+        return ("SUCCESS: You're in an isolated environments!\n"
+                "Safe to install packages without affecting\n"
+                "the global system.")
+
+
 if __name__ == "__main__":
-    venv_check = in_venv()
-    print(matrix_stat(venv_check))
+    print()
+    print(matrix_stat(in_venv()))
+    print()
+    print(f"Current Python: {current_path()}")
+    print()
+    print(f"{confirm_message(in_venv())}")
+    print()
+    print(package_site_path(in_venv()))
