fcf6b8cb213db934444cf4fba160c8fedda9dddb
Ladook <76804681+ninemoonz@users.noreply.github.com>
2026-09-18 16:51:56 +0200

    model_validator

--- py_module_09/ex1/alien_contact.py
+++ py_module_09/ex1/alien_contact.py
@@ -8,7 +8,7 @@ class ContactType(Enum):
     RADIO = "radio"
     VISUAL = "visual"
     PHYSICAL = "physical"
-    TELEPHATIC = "telephatic"
+    TELEPATIC = "telepatic"
 
 
 class AlienContact(BaseModel):
@@ -21,6 +21,18 @@ class AlienContact(BaseModel):
     witness_count: int = Field(..., ge=1, le=100)
     message_received: Optional[str] = Field(max_length=500)
     is_verified: bool = Field(default=False)
+
+    @model_validator(mode='after')
+    def business_rules(self) -> 'AlienContact':
+        if self.contact_id[:2] != "AC":
+            raise ValueError("contact_id should start with 'AC'.")
+        if self.contact_type == ContactType.PHYSICAL and not self.is_verified:
+            raise ValueError("Can not verify the physical contact.")
+        if self.contact_type == ContactType.TELEPATIC and not self.witness_count >= 3:
+            raise ValueError("Not enough witness to verify Telepathic contact.")
+        if self.signal_strength >= 7.0 and not self.message_recieved:
+            raise ValueError("Signal strneght is strong. Check message.")
+        return self
 
 
 def main() -> None:
@@ -46,6 +58,10 @@ def main() -> None:
     print(f"Witnesses: {contact_a.witness_count}")
     if contact_a.message_received:
         print(f"Message: {contact_a.message_received}")
+    if contact_a.is_verified:
+        print("Verification: Verified")
+    else:
+        print("Verification: Not verified")
     print()
     print("============================")
     print("Expected validation error(s):")
@@ -55,7 +71,7 @@ def main() -> None:
                     timestamp=datetime.today(),
                     location="Area 42, Amsterdam, NL",
                     contact_type=ContactType.PHYSICAL.value,
-                    signal_strength=4.42,
+                    signal_strength=7.42,
                     duration_minutes=3,
                     witness_count=7,
                     message_received="asdfadfasdf",
